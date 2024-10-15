a00239ab1c4362fe13d6361f9bbbc8d166d94050
Filipe Carvalho <fcf0133@gmail.com>
2024-10-15 10:34:09 -0300

    First Commit

--- Sistema/lib/Appwrite/constantes.dart
+++ Sistema/lib/Appwrite/constantes.dart
@@ -1,5 +1,4 @@
-const String APPWRITE_ID = "66e0e93d002a0c9fab67";
-//const String APPWRITE_URL = "http://192.168.0.113/v1";
-const String APPWRITE_URL = "http://192.168.52.157/v1";
-const String APPWRITE_DATABASE = "66e7b1500029ba1792f7";
-const String APPWRITE_BUCKET = "66e862a9001e9a14e0fd";
+const String APPWRITE_ID = "";
+const String APPWRITE_URL = "";
+const String APPWRITE_DATABASE = "";
+const String APPWRITE_BUCKET = "";
